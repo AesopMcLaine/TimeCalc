ead955c7ff9a7c3a581a9cdf048964e4a44c7a55
Aesop <arrynmclean@gmail.com>
2018-05-19 02:43:50 -0400

    options loop for invalid commands

--- TimeCalc.py
+++ TimeCalc.py
@@ -2,9 +2,8 @@ import datetime
 from datetime import timedelta
 
 # intro
-print("Welcome to TimeCalc")
-
-print("Calculating Durations")
+print("Welcome to TimeCalc"
+      "Calculating Durations")
 
 clear = True
 while clear == True:
@@ -12,7 +11,7 @@ while clear == True:
     firstNumber = True
     while firstNumber == True:
         time_1 = input("Please enter a time [hh:mm:ss] including 0s: ")
-        if len(time_1) > 0 and len(time_1) >= 8 and time_1[-6] == ":" and time_1[-3] == ":" and int(time_1[-5:-3]) < 60 and int(time_1[-2:]) < 60:
+        if len(time_1) >= 8 and time_1[-6] == ":" and time_1[-3] == ":" and int(time_1[-5:-3]) < 60 and int(time_1[-2:]) < 60:
             hours_1 = time_1[:-6]
             minutes_1 = time_1[-5:-3]
             seconds_1 = time_1[-2:]
@@ -30,7 +29,7 @@ while clear == True:
         secondNumber = True
         while secondNumber == True:
             time_2 = input("Please enter a time to add [hh:mm:ss] including 0s: ")
-            if len(time_2) > 0 and len(time_2) >= 8 and time_2[-6] == ":" and time_2[-3] == ":" and int(time_2[-5:-3]) < 60 and int(time_2[-2:]) < 60:
+            if len(time_2) >= 8 and time_2[-6] == ":" and time_2[-3] == ":" and int(time_2[-5:-3]) < 60 and int(time_2[-2:]) < 60:
                 hours_2 = time_2[:-6]
                 minutes_2 = time_2[-5:-3]
                 seconds_2 = time_2[-2:]
@@ -46,10 +45,16 @@ while clear == True:
         print("The total duration is: " + str(total))
 
         # continue, clear, or exit
-        choice = input("Continue: Y | Clear: N | Exit: X: ")
-        if choice == "Y" or choice == "y":
-            time_1 = total
-        elif choice == "N" or choice == "n":
-            cont = False
-        elif choice == "X" or choice == "x":
-            quit()
+        options = True
+        while options == True:
+            choice = input("Continue: Y | Clear: N | Exit: X: ")
+            if choice == "Y" or choice == "y":
+                time_1 = total
+                options = False
+            elif choice == "N" or choice == "n":
+                cont = False
+                options = False
+            elif choice == "X" or choice == "x":
+                quit()
+            else:
+                print("Invalid Syntax")
