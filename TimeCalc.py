75d38e99fa53fd4fe2e3c8481915182d9de1b492
Aesop <arrynmclean@gmail.com>
2018-05-19 02:57:49 -0400

    cleaned up variables

--- TimeCalc.py
+++ TimeCalc.py
@@ -12,12 +12,9 @@ while clear == True:
     while firstNumber == True:
         time_1 = input("Please enter a time [hh:mm:ss] including 0s: ")
         if len(time_1) >= 8 and time_1[-6] == ":" and time_1[-3] == ":" and int(time_1[-5:-3]) < 60 and int(time_1[-2:]) < 60:
-            hours_1 = time_1[:-6]
-            minutes_1 = time_1[-5:-3]
-            seconds_1 = time_1[-2:]
-            hours_1 = int(hours_1)
-            minutes_1 = int(minutes_1)
-            seconds_1 = int(seconds_1)
+            hours_1 = int(time_1[:-6])
+            minutes_1 = int(time_1[-5:-3])
+            seconds_1 = int(time_1[-2:])
             firstNumber = False
         else:
             print("Invalid Syntax")
@@ -30,12 +27,9 @@ while clear == True:
         while secondNumber == True:
             time_2 = input("Please enter a time to add [hh:mm:ss] including 0s: ")
             if len(time_2) >= 8 and time_2[-6] == ":" and time_2[-3] == ":" and int(time_2[-5:-3]) < 60 and int(time_2[-2:]) < 60:
-                hours_2 = time_2[:-6]
-                minutes_2 = time_2[-5:-3]
-                seconds_2 = time_2[-2:]
-                hours_2 = int(hours_2)
-                minutes_2 = int(minutes_2)
-                seconds_2 = int(seconds_2)
+                hours_2 = int(time_2[:-6])
+                minutes_2 = int(time_2[-5:-3])
+                seconds_2 = int(time_2[-2:])
                 secondNumber = False
             else:
                 print("Invalid Syntax")
